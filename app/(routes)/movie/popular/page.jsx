0b3c62891c80a33b5cee7150c1ec389ef97a278a
banch0u <fikret.okis@gmail.com>
2023-12-06 06:32:50 +0300

    popular top rated and upcoming pages ready

--- app/(routes)/movie/popular/page.jsx
+++ app/(routes)/movie/popular/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import Wrapper from '../../../components/UI/wrapper';
+import Title from '../../../components/UI/title';
+import TitleCard from '../../../components/titleCard';
+import CustomPagination from '../../../components/customPagination';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPopularMovies } from '../../../redux/movies/popularSlice';
 
 const Popular = () => {
-  return <div>Popular</div>;
+  const [page, setPage] = useState('1');
+  const dispatch = useDispatch();
+  let popularMovies = useSelector((state) => state.popularMovies);
+  useEffect(() => {
+    popularMovies = null;
+    dispatch(getPopularMovies({ language: 'en-US', page: page }));
+  }, [dispatch, page]);
+  return (
+    <main>
+      <Wrapper>
+        <Title link={false}>Now Playing</Title>
+        <div className='grid  grid-cols-5 gap-8 py-6'>
+          {popularMovies?.data?.results?.map((item) => (
+            <TitleCard data={item} type={'movie'} />
+          ))}
+        </div>
+        <CustomPagination
+          totalPages={popularMovies?.data?.total_pages}
+          changePage={setPage}
+        />
+      </Wrapper>
+    </main>
+  );
 };
 
 export default Popular;
--- app/(routes)/movie/upcoming/page.jsx
+++ app/(routes)/movie/upcoming/page.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import Wrapper from '../../../components/UI/wrapper';
+import Title from '../../../components/UI/title';
+import TitleCard from '../../../components/titleCard';
+import CustomPagination from '../../../components/customPagination';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUpcoming } from '../../../redux/movies/upcomingSlice';
 
 const Upcoming = () => {
-  return <div>Upcoming</div>;
+  const [page, setPage] = useState('1');
+  const dispatch = useDispatch();
+  let upcomingMovies = useSelector((state) => state.upcomingMovies);
+  useEffect(() => {
+    upcomingMovies = null;
+    dispatch(getUpcoming({ language: 'en-US', page: page }));
+  }, [dispatch, page]);
+  return (
+    <main>
+      <Wrapper>
+        <Title link={false}>Now Playing</Title>
+        <div className='grid  grid-cols-5 gap-8 py-6'>
+          {upcomingMovies?.data?.results?.map((item) => (
+            <TitleCard data={item} type={'movie'} />
+          ))}
+        </div>
+        <CustomPagination
+          totalPages={upcomingMovies?.data?.total_pages}
+          changePage={setPage}
+        />
+      </Wrapper>
+    </main>
+  );
 };
 
 export default Upcoming;
